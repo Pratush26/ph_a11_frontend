6ed0c7366b3e00faa4a35f3b7b6a91203436cbd8
Pratush <pratushbarua26@gmail.com>
2025-12-10 16:10:46 +0600

    implemented lottifiles animation

--- src/Pages/Home/Payment/AfterPayment.jsx
+++ src/Pages/Home/Payment/AfterPayment.jsx
@@ -2,9 +2,12 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link, useSearchParams } from "react-router";
 import { showToast } from "../../../Utils/ShowToast";
+import Lottie from "lottie-react";
+import fireworks from '../../../assets/Fireworks.json'
+import failed from '../../../assets/payment_unsuccessful.json'
 
 export default function AfterPayment() {
-     const [paymentData, setPaymentData] = useState({})
+    const [paymentData, setPaymentData] = useState({})
     const [searchParams] = useSearchParams()
     const sessionId = searchParams.get("session_id");
     useEffect(() => {
@@ -12,18 +15,31 @@ export default function AfterPayment() {
         axios.patch(`${import.meta.env.VITE_SERVER}/boost-issuePriority`, { session_id: sessionId })
             .then(res => setPaymentData(res.data))
             .catch(err => {
-                showToast({type: "error", message: err.message || "Some error occur!"})
+                showToast({ type: "error", message: err.message || "Some error occur!" })
                 console.error(err)
             });
     }, [sessionId]);
-    
-    if(searchParams.get("success") === "false") return <h1 className="text-4xl text-secondary font-bold m-10 animate-bounce">Payment Unsuccessful!</h1>
+
+    if (searchParams.get("success") === "false") {
+        return (
+            <main className="w-full">
+                <h1 className="text-4xl text-center text-secondary font-bold m-10">Payment Unsuccessful!</h1>
+                <div className="w-5/6 max-w-xs mx-auto">
+                <Lottie animationData={failed} />
+                </div>
+            </main>
+        )
+    }
     return (
-        <main className="flex flex-col items-center justify-center gap-3 mx-4 my-8">
-            <h1 className="text-3xl text-secondary font-bold">Payment Successful!</h1>
-            {paymentData?.issueId && <p>Your issue's tracking Id: {paymentData?.issueId}</p>}
-            <p>You successfully paid {paymentData?.currency || "="} {paymentData?.cost || 0}</p>
-            <Link to='/' className="btn btn-primary rounded-lg trnsition shadow-md">Back to home</Link>
+        <main className="grid grid-cols-3 w-full min-h-[70vh] text-center mx-4 my-8">
+            <Lottie animationData={fireworks} />
+            <section className="flex flex-col items-center justify-center gap-3">
+                <h1 className="text-3xl text-secondary font-bold">Payment Successful!</h1>
+                {paymentData?.issueId && <p>Your issue's tracking Id: {paymentData?.issueId}</p>}
+                <p>You successfully paid {paymentData?.currency || "="} {paymentData?.cost || 0}</p>
+                <Link to='/' className="btn btn-primary rounded-lg trnsition shadow-md">Back to home</Link>
+            </section>
+            <Lottie animationData={fireworks} />
         </main>
     )
 }
